6b71e02b9a42448b3bf248175984c065bead9f8a
Diogo Mendonca <diogomedeirosmendonca@enta.pt>
2025-02-19 17:54:25 +0200

    refactor main to reduce lines

--- checker_bonus/main_bonus.c
+++ checker_bonus/main_bonus.c
@@ -6,7 +6,7 @@
 /*   By: dimendon <dimendon@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/19 15:36:09 by dimendon          #+#    #+#             */
-/*   Updated: 2025/02/19 17:02:05 by dimendon         ###   ########.fr       */
+/*   Updated: 2025/02/19 17:32:32 by dimendon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,11 +52,24 @@ void	execute_command(char *command, t_list **a, t_list **b)
 		rotate_both(a, b, 1);
 }
 
-void	free_all(t_list **a, t_list **b, char *args, char *command)
+void	free_all(t_list **a, t_list **b, char *args)
 {
 	ft_lstclear(a);
 	ft_lstclear(b);
 	free(args);
+}
+
+void	read_and_execute(t_list **a, t_list **b)
+{
+	char	*command;
+
+	command = get_next_line(0);
+	while (command != NULL)
+	{
+		execute_command(command, a, b);
+		command = get_next_line(0);
+	}
+	free(command);
 }
 
 int	main(int argC, char *argV[])
@@ -64,26 +77,21 @@ int	main(int argC, char *argV[])
 	t_list	*a;
 	t_list	*b;
 	char	*args_str;
-	char	*command;
 
 	a = NULL;
 	b = NULL;
 	args_str = concat_args(argV, argC);
-	if (validate_args(args_str) == 1 || initialize_list(args_str, &a) == 1)
+	if (validate_args(args_str) == 1 || initialize_list(args_str, &a) == 1
+		|| check_duplicates(a) == 1)
 		write(1, "Error\n", 7);
 	else
-	{
-		command = get_next_line(0);
-		while (command != NULL)
-		{
-			execute_command(command, &a, &b);
-			command = get_next_line(0);
-		}
+	{	
+		read_and_execute(&a, &b);
 		if (is_sorted(a) && b == NULL)
 			write(1, "OK\n", 3);
 		else
 			write(1, "KO\n", 3);
 	}
-	free_all(&a, &b, args_str, command);
+	free_all(&a, &b, args_str);
 	return (0);
 }
